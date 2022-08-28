dd35cc75b345c2e38e2a04f9b529c7da67e2a303
Ishan-Mitra <backup.ishanfiles@gmail.com>
2022-08-28 19:45:07 +0530

    stable

--- drivers/ytd.py
+++ drivers/ytd.py
@@ -1,11 +1,8 @@
 from pytube import YouTube, request
 
 def yt_download(url: str):
-    yt = YouTube(url).streams.get_highest_resolution()
-    stream = request.stream(yt.url)
-    while True:
+    stream = request.stream(YouTube(url).streams.get_highest_resolution().url)
+    chunk = next(stream, None)
+    while chunk:
+        yield chunk
         chunk = next(stream, None)
-        if chunk:
-            yield chunk
-        else:
-            break
